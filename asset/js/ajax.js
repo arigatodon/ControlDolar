9107d2e8b4361d7d03610d6d6aedd253be4d86c8
igor venegas <venegas.igor@gmail.com>
2015-09-20 20:33:56 -0300

    se le agrego el valor por cuota + fuente de googlefont + y un background responsivo

--- asset/js/ajax.js
+++ asset/js/ajax.js
@@ -14,10 +14,9 @@ $('#btn').click(function(e) {
 		var prestamo_pesos = data.uf.valor * valor ;
 		var interes = cantidad * 0.015;
 		var valor_total = prestamo_pesos+prestamo_pesos*interes;
-		var html = '<h4>valor UF : '+data.uf.valor+'</h4>';
-			html +='<h4>DOlaar : $'+data.dolar.valor+'</h4>';
-			html +='<h4>valor en pesos del prestamo: $'+ parseInt(prestamo_pesos) +'</h4>';
+		var html ='<h4>valor en pesos del prestamo: $'+ parseInt(prestamo_pesos) +'</h4>';
 			html +='<h4> valor Total a pagar : $ '+ parseInt(valor_total)+'</h4>' ;
+			html +="<h4> el valor por cuota es de : $"+ parseInt(valor_total /cantidad )+" </h4>";
 		$elem.html(html);
 	})
 	.fail(function() {
